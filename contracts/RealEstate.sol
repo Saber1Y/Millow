e791637f6c158e44b6e7a2c562b25d4b47afd1a0
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-09 22:12:55 +0100

    UPDATE:NFT CONTRACT

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,6 +1,25 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract RealEstate {
+contract RealEstate is ERC721URIStorage {
+   string Counters for Counter.Counter;
+   Counters.Counter private _tokenIds;
 
+   constructor() ERC721("RealEstate", "REAL") {}
+
+   function mint(string memory _tokenURI) public  returns (uint256) {
+     _tokenIds.increament();
+
+     uint256 newItemId = _tokenIds.current();
+     _mint(msg.sender, newItemId);
+     _setTokenURI(newItemId, _tokenURI);
+
+     return newItemId;
+   }
+   function totalSupply() public view returns (uint256) {
+       return _tokenIds.current();
+   }
 }
